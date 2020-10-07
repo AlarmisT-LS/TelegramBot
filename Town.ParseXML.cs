1c0ac9396bffdded8a765e959000dde726dab5fa
AlarmisT <71372904+AlarmisT-LS@users.noreply.github.com>
2020-10-07 03:24:24 +0300

    Add files via upload

---
+++ Town.ParseXML.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Xml;
+
+namespace ConsoleAppZet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Town list = new Town();
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("https://xml.meteoservice.ru/export/gismeteo/point/148.xml");
+            //xDoc.Load("D:\\Fun\\XMLFile1.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            //--------Town
+            XmlNode vals = xRoot.SelectSingleNode("//MMWEATHER/REPORT/TOWN");
+            list.Index = int.Parse(vals.SelectSingleNode("@index").Value);
+            list.Sname = vals.SelectSingleNode("@sname").Value;
+            list.Latitude = int.Parse(vals.SelectSingleNode("@latitude").Value);
+            list.Longitude = int.Parse(vals.SelectSingleNode("@longitude").Value);
+            //--------FORECAST
+            XmlNodeList nodelist = vals.SelectNodes("*");
+            int j = 0; //Счётчик
+            foreach (XmlNode i in nodelist)
+            {
+                //-FORECAST-Attributes
+                list.forecast[j].Day = int.Parse(i.SelectSingleNode("@day").Value);
+                list.forecast[j].Month = int.Parse(i.SelectSingleNode("@month").Value);
+                list.forecast[j].Year = int.Parse(i.SelectSingleNode("@year").Value);
+                list.forecast[j].Hour = int.Parse(i.SelectSingleNode("@hour").Value);
+                list.forecast[j].Tod = int.Parse(i.SelectSingleNode("@tod").Value);
+                list.forecast[j].Predict = int.Parse(i.SelectSingleNode("@predict").Value);
+                list.forecast[j].Weekday = int.Parse(i.SelectSingleNode("@weekday").Value);
+                //--FORECAST-PHENOMENA-Attributes
+                XmlNode xml = i.SelectSingleNode("PHENOMENA");
+
+                list.forecast[j].phenomena.Cloudiness = int.Parse(xml.SelectSingleNode("@cloudiness").Value);
+                list.forecast[j].phenomena.Precipitation = int.Parse(xml.SelectSingleNode("@precipitation").Value);
+                list.forecast[j].phenomena.Rpower = int.Parse(xml.SelectSingleNode("@rpower").Value);
+                list.forecast[j].phenomena.Spower = int.Parse(xml.SelectSingleNode("@spower").Value);
+                //--FORECAST-PRESSURE-Attributes
+                xml = i.SelectSingleNode("PRESSURE");
+                list.forecast[j].pressure.Max = int.Parse(xml.SelectSingleNode("@max").Value);
+                list.forecast[j].pressure.Min = int.Parse(xml.SelectSingleNode("@min").Value);
+                //--FORECAST-TEMPERATURE-Attributes
+                xml = i.SelectSingleNode("TEMPERATURE");
+                list.forecast[j].temperature.Max = int.Parse(xml.SelectSingleNode("@max").Value);
+                list.forecast[j].temperature.Min = int.Parse(xml.SelectSingleNode("@min").Value);
+                //--FORECAST-WIND-Attributes
+                xml = i.SelectSingleNode("WIND");
+                list.forecast[j].wind.Max = int.Parse(xml.SelectSingleNode("@max").Value);
+                list.forecast[j].wind.Min = int.Parse(xml.SelectSingleNode("@min").Value);
+                list.forecast[j].wind.Direction = int.Parse(xml.SelectSingleNode("@direction").Value);
+                //--FORECAST-RELWET-Attributes
+                xml = i.SelectSingleNode("RELWET");
+                list.forecast[j].relwet.Max = int.Parse(xml.SelectSingleNode("@max").Value);
+                list.forecast[j].relwet.Min = int.Parse(xml.SelectSingleNode("@min").Value);
+                //--FORECAST-HEAT-Attributes
+                xml = i.SelectSingleNode("HEAT");
+                list.forecast[j].heat.Max = int.Parse(xml.SelectSingleNode("@max").Value);
+                list.forecast[j].heat.Min = int.Parse(xml.SelectSingleNode("@min").Value);
+                j++;
+            }
+            //Console.WriteLine(xml.OuterXml);
+        }
+
+    }
+}
+
---
+++ Town.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppZet
+{
+    class Town
+    {
+        public int Index { get; set; }
+        public string Sname { get; set; }
+        public int Latitude { get; set; }
+        public int Longitude { get; set; }
+        private const int size = 4;
+        public List<Forecast> forecast;
+
+
+        public Town()
+        {
+            Forecast temp;
+            forecast = new List<Forecast>();
+            for (int i = 0; i < size; i++)
+            {
+                temp = new Forecast();
+                forecast.Add(temp);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                forecast[i].phenomena = new Phenomena();
+                forecast[i].pressure = new Pressure();
+                forecast[i].temperature = new Temperature();
+                forecast[i].wind = new Wind();
+                forecast[i].relwet = new Relwet();
+                forecast[i].heat = new Heat();
+            }
+
+        }
+    }
+    class Forecast
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public int Hour { get; set; }
+        public int Tod { get; set; }
+        public int Predict { get; set; }
+        public int Weekday { get; set; }
+
+
+
+        public Phenomena phenomena;
+        public Pressure pressure;
+        public Temperature temperature;
+        public Wind wind;
+        public Relwet relwet;
+        public Heat heat;
+        public Forecast()
+        {
+            phenomena = new Phenomena();
+            pressure = new Pressure();
+            temperature = new Temperature();
+            wind = new Wind();
+            relwet = new Relwet();
+            heat = new Heat();
+        }
+
+
+    }
+    class Phenomena
+    {
+        public int Cloudiness { get; set; }
+        public int Precipitation { get; set; }
+        public int Rpower { get; set; }
+        public int Spower { get; set; }
+
+    }
+    class Pressure
+    {
+        public int Max { get; set; }
+        public int Min { get; set; }
+
+    }
+    class Temperature
+    {
+        public int Max { get; set; }
+        public int Min { get; set; }
+    }
+    class Wind
+    {
+        public int Max { get; set; }
+        public int Min { get; set; }
+        public int Direction { get; set; }
+    }
+    class Relwet
+    {
+        public int Max { get; set; }
+        public int Min { get; set; }
+    }
+    class Heat
+    {
+        public int Max { get; set; }
+        public int Min { get; set; }
+    }
+}
